018d6de8530ab455dbb5ecf8b7a71135ccad4db1
Arsh2 <pathanarshkhan@gmail.com>
2023-03-09 12:38:16 +0530

    Added Custom Hooks Folder

---
+++ Building First Custom hook/src/components/ForwardCounter.js
@@ -0,0 +1,11 @@
+import useCounter from "../hooks/use-counter";
+
+import Card from "./Card";
+
+const ForwardCounter = () => {
+  const counter = useCounter();
+
+  return <Card>{counter}</Card>;
+};
+
+export default ForwardCounter;
---
+++ Building First Custom hook/src/hooks/use-counter.js
@@ -0,0 +1,18 @@
+import React, { useEffect, useState } from "react";
+
+const useCounter = (forwards = true) => {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (forwards) setCounter((prevCounter) => prevCounter + 1);
+      else setCounter((prevCounter) => prevCounter - 1);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return counter;
+};
+
+export default useCounter;
